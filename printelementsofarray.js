04e6d16546df3ba02836d580bf0e753565087e12
techlojutt <muhammadshoaib2140jutt@gmail.com>
2024-07-07 16:55:29 +0500

    first commit

---
+++ printelementsofarray.js
@@ -0,0 +1,9 @@
+function readAndPrintElementsofAnArray(arr)
+{
+  for (let index = 0; index < arr.length; index++) 
+   {
+      console.log(array[index]);
+   }
+}
+var array = ["Shoaib","Bilal","Saad","Umair","Tibiyan","Saljok"];
+readAndPrintElementsofAnArray(array);
